17e2fe2c5e378bbbe548e8238ff663b8f8253c45
AnimacionLosEnlaces <animacionlosenlaces@gmail.com>
2017-04-26 17:54:26 +0200

    material SQL relacional

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "prau";
+$dbname = "inaem_prau";
 
 /*
 
--- includes/get_material_data.php
+++ includes/get_material_data.php
@@ -13,7 +13,12 @@ $row = array();
 
 if(isset($_GET['m']) && $_GET['m'] != '') {
 	//Consulta SQL compleja que devuelve los datos del usuario y de los ciclos en los que está inscrito
-	$sql = "SELECT * FROM materiales WHERE codigo_material = '" . $_GET['m'] . "' LIMIT 1";
+	//$sql = "SELECT * FROM materiales WHERE codigo_material = '" . $_GET['m'] . "' LIMIT 1";
+	$sql = "SELECT M.*, C.nombre AS categoria, C.id_categoria, S.foto, S.nombre AS subcategoria 
+		FROM materiales M, categorias C, subcategorias S 
+		WHERE M.codigo_material = '" . $_GET['m'] . "' 
+		AND M.id_subcategoria = S.id_subcategoria 
+		AND S.id_categoria = C.id_categoria";
 	//Ejecuto la consulta SQL
 	$result = mysqli_query($conn, $sql);
 	
